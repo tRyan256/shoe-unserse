9c7d1b6efd4c35fee8c1eef25b568c97be7bccfb
“tRyan” <tRyan050727@outlook.com>
2026-03-16 17:58:03 +0800

    feat: add L1 cache for airdrop meta

--- su-server/src/main/java/com/su/config/LocalCacheConfiguration.java
+++ su-server/src/main/java/com/su/config/LocalCacheConfiguration.java
@@ -3,6 +3,7 @@ package com.su.config;
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.su.entity.Category;
+import com.su.service.airdrop.support.AirdropMeta;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,6 +17,14 @@ public class LocalCacheConfiguration {
         return Caffeine.newBuilder()
                 .maximumSize(100)
                 .expireAfterWrite(Duration.ofSeconds(30))
+                .build();
+    }
+
+    @Bean
+    public Cache<Long, AirdropMeta> airdropMetaLocalCache() {
+        return Caffeine.newBuilder()
+                .maximumSize(5000)
+                .expireAfterWrite(Duration.ofSeconds(10))
                 .build();
     }
 }
